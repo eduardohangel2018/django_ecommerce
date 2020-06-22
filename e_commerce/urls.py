dfcd90fe38bee1c36b62b51230a444b6215d4838
System Administrator <root@MacBook-Air-de-Eduardo.local>
2020-06-21 23:06:09 -0300

    input imports and inserts utrl patterns, about_page and contact_page

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 from .views import home_page
 urlpatterns = [
         path('', home_page),
+        path('about/', about_page),
+        path('contact/', contact_page),
         path('admin/', admin.site.urls),
 ]
